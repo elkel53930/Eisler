161da3b4573161a94d76006b792989a4532fb5dd
elkel53930 <elkel53930@gmail.com>
2017-05-20 10:19:30 +0900

    SummaryにBOM出力を追加

--- src/Summary.hs
+++ src/Summary.hs
@@ -6,6 +6,11 @@ import Common
 import qualified Data.Set as Set
 import Data.List
 import Data.Time
+import Control.Applicative
+import Network.HTTP.Base (urlEncode)
+
+
+type Markdown = String
 
 output :: FilePath -> [Net] -> IO()
 output fp net = do
@@ -17,11 +22,61 @@ output fp net = do
     , "] Translation Summary\n\n"
     , timeInfo
     , "\n\n"
+    , bom net
     , partsInformation net
     , netsInformation net
     ]
 
-partsInformation :: [Net] -> String
+{-
+  BOMを出力
+  リファレンス、型番、数量、Digikeyで検索、RSで検索、Chip1Stopで検索
+-}
+
+
+bom :: [Net] -> Markdown
+bom =
+  (++) "## BOM\n\n|Ref.|Type|Qty|Search|\n|--|--|--|--|--|\n"
+  . concatMap showBom
+  . groupBy (\(pt1,_) (pt2,_) -> pt1 == pt2)
+  . nub
+  . sort
+  . map (\(ConPort _ _ _ ref _ pt) -> (pt,ref))
+  . filter isConPort
+  . Set.elems
+  . foldl Set.union Set.empty
+  . map (\x -> snd . getNet $ x)
+
+showBom :: [(Maybe PartType, Reference)] -> String
+showBom all@((pt,_):xs) = concat
+  [ "|", concat . intersperse " " $ map snd all -- References
+  , "|", mb $ getToken <$> pt                   -- Part Type
+  , "|", show $ length all                      -- Qty.
+  , "|", mb $ rsSearch <$> pt                   -- RS
+  , ", ", mb $ digiKeySearch <$> pt              -- DigiKey
+  , ", ", mb $ chip1Search <$> pt                -- Chip1Stop
+  , "|\n"
+  ] where mb x = case x of Nothing -> ""; Just t -> t;
+
+makeSearch :: String -> String -> PartType -> Markdown
+makeSearch title url part = concat
+  [ "["
+  , title
+  , "]("
+  , url
+  , urlEncode $ getToken part
+  , ")"
+  ]
+
+digiKeySearch = makeSearch "Digikey" "https://www.digikey.jp/products/ja?keywords="
+rsSearch = makeSearch "RS" "http://jp.rs-online.com/web/c/?sra=oss&r=t&searchTerm="
+chip1Search = makeSearch "Chip1Stop" "http://www.chip1stop.com/search.do?classCd=&did=&keyword="
+
+{-
+  パーツインフォメーション
+  eisファイル内のどのpartのリファレンスがいくつで、その型番がなんであるかの一覧
+-}
+
+partsInformation :: [Net] -> Markdown
 partsInformation =
   (++) "## Parts information\n\n|Ref.|Decl.|Type|\n|--|--|--|\n"
   . concatMap showPart
@@ -38,7 +93,7 @@ isConPort x = case x of
   ConPort _ _ _ _ _ _ -> True
   otherwise -> False
 
-showPart :: (Reference,CompIden,Maybe PartType) -> String
+showPart :: (Reference,CompIden,Maybe PartType) -> Markdown
 showPart (r,c,pt) = concat
   [ "|", r
   , "|", atmark $ getToken c
@@ -48,14 +103,19 @@ showPart (r,c,pt) = concat
   , "|\n"
   ]
 
-netsInformation :: [Net] -> String
+{-
+  ネットインフォメーション
+  ネット名と、そのネットに繋がっているピンの一覧
+-}
+
+netsInformation :: [Net] -> Markdown
 netsInformation =
   (++) "## Nets information\n\n|Net name|Pins|\n|--|--|\n"
   . concat
   . sort
   . map showNet
 
-showNet :: Net -> String
+showNet :: Net -> Markdown
 showNet net = concat
   [ "|", atmark $ getToken wire
   , "|", showConnectables set
@@ -63,7 +123,7 @@ showNet net = concat
   ] where
     (wire,set) = getNet net
 
-showConnectables :: Set.Set Connectable -> String
+showConnectables :: Set.Set Connectable -> Markdown
 showConnectables =
   concat
   . intersperse "<br>"
@@ -72,16 +132,13 @@ showConnectables =
   . map showConnectable
   . Set.elems
 
-showConnectable :: Connectable -> String
+showConnectable :: Connectable -> Markdown
 showConnectable (ConPort c _ po _ _ _) = concat
   [ atmark $ getToken c
   , " . "
   , getToken po
   ]
 showConnectable _ = ""
-{-
-showConnectable (ConWire w) =  "Wire : " ++ (atmark $ getToken w)
-showConnectable (ConItfc i) =  "Interface : " ++ (atmark $ getToken i)
--}
+
 atmark :: String -> String
 atmark = replace '-' '@'
--- src/testEis.md
+++ src/testEis.md
@@ -1,7 +1,14 @@
 # [testEis.eis] Translation Summary
 
-2017-05-19 16:46:05.2111418 ìåãû (ïWèÄéû)
+2017-05-20 10:17:48.6839463 ìåãû (ïWèÄéû)
 
+## BOM
+
+|Ref.|Type|Qty|Search|
+|--|--|--|--|--|
+|R1 R2|100 5% 1608|2|[RS](http://jp.rs-online.com/web/c/?sra=oss&r=t&searchTerm=100%205%25%201608), [Digikey](https://www.digikey.jp/products/ja?keywords=100%205%25%201608), [Chip1Stop](http://www.chip1stop.com/search.do?classCd=&did=&keyword=100%205%25%201608)|
+|CN1|B2B-EH|1|[RS](http://jp.rs-online.com/web/c/?sra=oss&r=t&searchTerm=B2B-EH), [Digikey](https://www.digikey.jp/products/ja?keywords=B2B-EH), [Chip1Stop](http://www.chip1stop.com/search.do?classCd=&did=&keyword=B2B-EH)|
+|LD1 LD2 LD3 LD4|BR1111C|4|[RS](http://jp.rs-online.com/web/c/?sra=oss&r=t&searchTerm=BR1111C), [Digikey](https://www.digikey.jp/products/ja?keywords=BR1111C), [Chip1Stop](http://www.chip1stop.com/search.do?classCd=&did=&keyword=BR1111C)|
 ## Parts information
 
 |Ref.|Decl.|Type|
@@ -21,5 +28,5 @@
 |$$$00002|l0@l0@main . K<br>l1@l0@main . A|
 |$$$00003|l0@l1@main . A<br>r1@main . _2|
 |$$$00004|l0@l1@main . K<br>l1@l1@main . A|
-|GND-main|cn@main . GND<br>l1@l0@main . K<br>l1@l1@main . K|
-|VCC-main|cn@main . VCC<br>r0@main . _1<br>r1@main . _1|
+|GND@main|cn@main . GND<br>l1@l0@main . K<br>l1@l1@main . K|
+|VCC@main|cn@main . VCC<br>r0@main . _1<br>r1@main . _1|
