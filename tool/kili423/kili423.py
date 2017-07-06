71ab350ee1f73ee7bd0bb990013e428f028f0f88
Kazuki Iida <kazuki.iida@liferobotics.jp>
2017-07-06 20:23:57 +0900

    kili423(KiCad4向けフットプリントファイルkicad_modを旧KiCad向けに変換するツール)を追加

---
+++ tool/kili423/kili423.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.1
+import sys
+
+src = sys.argv[1]
+dst = src.replace(".kicad_mod","old.kicad_mod")
+with open(src,'r') as f:
+    srcMod = f.read();
+f.closed
+srcMod = srcMod.replace('F.Fab','Eco1.User')
+srcMod = srcMod.replace('B.Fab','Eco1.User')
+srcMod = srcMod.replace('F.CrtYd','Eco2.User')
+srcMod = srcMod.replace('B.CrtYd','Eco2.User')
+print(srcMod)
+with open(dst,'w') as f:
+    f.write(srcMod)
+f.closed
