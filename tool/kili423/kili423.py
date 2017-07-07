a2af7ffd81c11357e90ae33dbc09e47aed5d5089
Kazuki Iida <kazuki.iida@liferobotics.jp>
2017-07-07 09:48:25 +0900

    不要なf.closedを削除

--- tool/kili423/kili423.py
+++ tool/kili423/kili423.py
@@ -5,7 +5,6 @@ src = sys.argv[1]
 dst = src.replace(".kicad_mod","old.kicad_mod")
 with open(src,'r') as f:
     srcMod = f.read();
-f.closed
 srcMod = srcMod.replace('F.Fab','Eco1.User')
 srcMod = srcMod.replace('B.Fab','Eco1.User')
 srcMod = srcMod.replace('F.CrtYd','Eco2.User')
@@ -13,4 +12,3 @@ srcMod = srcMod.replace('B.CrtYd','Eco2.User')
 print(srcMod)
 with open(dst,'w') as f:
     f.write(srcMod)
-f.closed
